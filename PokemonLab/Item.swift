1fdfe11e21d3b53442663604e92a48feca2d35be
Hamza EL Aidi <elaidi.hamza@gmail.com>
2026-04-23 02:55:13 +0200

    chore: initial PokemonLab project setup

---
+++ PokemonLab/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PokemonLab
+//
+//  Created by hamza el aidi on 23/04/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PokemonLabTests/PokemonLabTests.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonLabTests.swift
+//  PokemonLabTests
+//
+//  Created by hamza el aidi on 23/04/2026.
+//
+
+import Testing
+@testable import PokemonLab
+
+struct PokemonLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
